e436354437c44ae4dbc47ee8f41673e09d727c40
deadlock <909973216@qq.com>
2020-08-11 12:24:36 +0800

    修改test方法

--- GitProject/src/com/edu/git.java
+++ GitProject/src/com/edu/git.java
@@ -4,9 +4,11 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
-    public void test(){
-            int i = 0;
-            i += 10;
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
     }
 }
 
